523b0c71c9c19e26735209e5fac3ea19c54907ba
Karol Piątek <karol.piatek@yahoo.com>
2020-11-25 21:51:58 +0100

    navi part 3

--- js/main.js
+++ js/main.js
@@ -2,17 +2,12 @@ document.addEventListener('DOMContentLoaded', function(){
 const nav = document.querySelector('.navi__items')
 const hamburgerBtn = document.querySelector(".hamburger")
 const allNaviItems = document.querySelectorAll('.navi__item')
-const navBtnBars = document.querySelector('.burger-btn__bars')
-
-// function handleNavi(){
-//     nav.classList.toggle('showNavi')
-//     hamburgerBtn.classList.toggle('showNavi')
-// }
+const navShadow = document.querySelector('.header-wrapper')
 
 // // SHOW MENU
 const handleNavi = () => {
     nav.classList.toggle('showNavi')
-    // hamburgerBtn.classList.toggle('ahover')
+
     allNaviItems.forEach(item => {
         item.addEventListener('click', () => {
             nav.classList.remove('showNavi')
@@ -23,14 +18,13 @@ const handleNavi = () => {
 hamburgerBtn.addEventListener('click', handleNavi)
 
 // // SHADOW NAVI
-// function addShadow(){
-//     if (window.scrollY >= 20){
-//         nav.classList.add('shadow-bg')
-//     }else{
-//         nav.classList.remove('shadow-bg')
-//     }
-// }
-// window.addEventListener('scroll',addShadow)
-// hamburgerBtn.addEventListener('click', handleNav);
+function addShadowNavi(){
+    if (window.scrollY >= 20){
+        navShadow.classList.add('shadow-bg')
+    }else{
+        navShadow.classList.remove('shadow-bg')
+    }
+}
+window.addEventListener('scroll',addShadowNavi)
 
 })
